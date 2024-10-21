6c5a9ad99520b3d5ed0326e53ab1b34f32013f8a
PARANTHAMAN (BARATH) <vbarath210706@gmail.com>
2024-10-22 00:47:24 +0530

    Update pretty_table.py

--- pretty_table.py
+++ pretty_table.py
@@ -1,7 +1,7 @@
 from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type",["Electric", "Water", "Fire"])
+table.add_column("Type",["Electric", "Water", "Grass"])
 
 print(table.align)
 
